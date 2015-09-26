7226486deb8132e1c7de7c268a611efed5d99422
avara1986 <a.vara.1986@gmail.com>
2015-09-26 19:34:24 +0200

    Added cookies to user controllers

--- front/src/app/user/user.controller.js
+++ front/src/app/user/user.controller.js
@@ -6,25 +6,9 @@
     .controller('UserController', UserController);
 
   /** @ngInject */
-  function UserController($timeout, $scope, $http, apiurl) {
-    var vm = this;
-    $scope.user = {
-      username: 'admin',
-      password: 'admin'
-    };
-    $scope.submit = function() {
-        $scope.master = angular.copy($scope.user);
-        console.log($scope.master);
-        $http.post(apiurl+'/api-token-auth/', $scope.master)
-        .success(function(data) {
-            console.log(data);
-        }).
-        error(function(data, status, headers, config) {
-            console.log(data);
-            console.log(status);
-            console.log(headers);
-            console.log(config);
-        });
-    }
+  function UserController($timeout, $rootScope, $http, $cookies, apiurl) {
+	$rootScope.is_login = $cookies.get('is_login');
+	//console.log($rootScope.is_login);
+	$rootScope.user = $cookies.getObject('user');
   }
 })();
