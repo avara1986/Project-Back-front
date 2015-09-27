39627dd0ba73921727747d8db8b8407e15182d0f
avara1986 <a.vara.1986@gmail.com>
2015-09-27 17:21:58 +0200

    Create forms send html and url

---
+++ front/src/app/email/mail.controller.js
@@ -0,0 +1,90 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('front')
+    .controller('EmailUrlController', EmailUrlController)
+    .controller('EmailHtmlController', EmailHtmlController)
+    .controller('EmailHistoryController', EmailHistoryController);
+
+  /** @ngInject */
+  function EmailUrlController($timeout, $rootScope, $http, $cookies, $mdToast, apiurl) {
+	this.email = {
+			to: [],
+	}
+
+	this.reset = function() {
+		this.email = {
+				to: [],
+		}
+	}
+	this.submit = function(email) {
+        $http.post(apiurl+'emails/send-url/', [email, $rootScope.user])
+        .success(function(data) {
+            console.log(data);
+            $mdToast.show(
+          	      $mdToast.simple()
+          	        .content('Email sent Ok! ;)')
+          	        .position('top right')
+          	        .hideDelay(3000)
+          	    );
+        }).
+        error(function(data, status, headers, config) {
+            console.log(data);
+            console.log(status);
+            console.log(headers);
+            console.log(config);
+            $mdToast.show(
+            	      $mdToast.simple()
+            	        .content('Backend Error!')
+            	        .position('top right')
+            	        .hideDelay(3000)
+            	    );
+        });
+
+    }
+  }
+  /** @ngInject */
+  function EmailHtmlController($timeout, $rootScope, $http, $cookies, apiurl) {
+		this.email = {
+				to: [],
+		}
+
+		this.reset = function() {
+			this.email = {
+					to: [],
+			}
+		}
+		this.submit = function(emai) {
+	        $http.post(apiurl+'emails/send-html/', emai)
+	        .success(function(data) {
+	            console.log(data);
+	            $mdToast.show(
+	          	      $mdToast.simple()
+	          	        .content('Email sent Ok! ;)')
+	          	        .position('top right')
+	          	        .hideDelay(3000)
+	          	    );
+	        }).
+	        error(function(data, status, headers, config) {
+	            console.log(data);
+	            console.log(status);
+	            console.log(headers);
+	            console.log(config);
+	            $mdToast.show(
+	            	      $mdToast.simple()
+	            	        .content('Backend Error!')
+	            	        .position('top right')
+	            	        .hideDelay(3000)
+	            	    );
+	        });
+
+	    }
+	  }
+  /** @ngInject */
+  function EmailHistoryController($timeout, $rootScope, $http, $cookies, apiurl) {
+	$rootScope.is_login = $cookies.get('is_login');
+	//console.log($rootScope.is_login);
+	$rootScope.user = $cookies.getObject('user');
+  }
+})();
