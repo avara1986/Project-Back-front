b004c4dd8a127fee4532f2a860bd7f90bbb8b891
avara1986 <a.vara.1986@gmail.com>
2015-09-29 22:56:11 +0200

    Update email serializer

--- back/emails/serializers.py
+++ back/emails/serializers.py
@@ -5,17 +5,29 @@ from emails.models import Contact, Email
 class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = ('email')
+        fields = ('email',)
 
 
 class EmailSerializer(serializers.ModelSerializer):
     to = ContactSerializer(many=True, required=True)
 
+    def create(self, validated_data):
+        tos = validated_data.pop('to')
+
+        email = Email.objects.create(**validated_data)
+
+        for to in tos:
+            con, result = Contact.objects.get_or_create(email=to['email'])
+            email.to.add(con)
+
+        return email
+
     class Meta:
         model = Email
         fields = (
-                  'to',
                   'url',
+                  'to',
+                  'user',
                   'subject',
                   'content',
                   )
