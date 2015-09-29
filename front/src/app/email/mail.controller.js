f11c8a01c8ccf4f58965a264f98caba90e37afb7
avara1986 <a.vara.1986@gmail.com>
2015-09-29 22:56:54 +0200

    Update front URL to api send email url

--- front/src/app/email/mail.controller.js
+++ front/src/app/email/mail.controller.js
@@ -19,7 +19,7 @@
 		}
 	}
 	this.submit = function(email) {
-        $http.post(apiurl+'emails/send-url/', {'email': email, 'g_token': $cookies.get('g_token')})
+        $http.post(apiurl+'emails/sendurl/', {'email': email, 'g_token': $cookies.get('g_token')})
         .success(function(data) {
             console.log(data);
             $mdToast.show(
