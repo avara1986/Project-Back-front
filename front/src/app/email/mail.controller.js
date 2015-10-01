81d6ecd0d98e65eb01ab760e7dfacf51f65bd7c9
avara1986 <a.vara.1986@gmail.com>
2015-10-01 21:26:12 +0200

    Update email Controller

--- front/src/app/email/mail.controller.js
+++ front/src/app/email/mail.controller.js
@@ -18,8 +18,24 @@
 				to: [],
 		}
 	}
+	this.parseData = function(data, token){
+		var ndata = new Object();
+		ndata.url = data.url;
+		ndata.subject = data.subject;
+		ndata.g_token = token;
+		ndata.to = [];
+		for (var i = 0; i < data.to.length; i++) { 
+			var aux = new Object();
+			console.log(data.to[i]);
+			aux.email = data.to[i];
+			
+			ndata.to.push(aux);
+		}
+		return ndata;
+	}
 	this.submit = function(email) {
-        $http.post(apiurl+'emails/sendurl/', {'email': email, 'g_token': $cookies.get('g_token')})
+		$rootScope.show_form_error=false;
+        $http.post(apiurl+'emails/sendurl/', this.parseData(email, $cookies.get('g_token')))
         .success(function(data) {
             console.log(data);
             $mdToast.show(
@@ -34,6 +50,8 @@
             console.log(status);
             console.log(headers);
             console.log(config);
+            $rootScope.show_form_error=true;
+            $rootScope.form_error_msg=data;
             $mdToast.show(
             	      $mdToast.simple()
             	        .content('Backend Error!')
