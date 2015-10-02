a9be8e7e4bb7874a29c5ac7dc95bb0c36967d54b
avara1986 <a.vara.1986@gmail.com>
2015-10-02 12:02:54 +0200

    Update front design

--- front/src/app/index.run.js
+++ front/src/app/index.run.js
@@ -6,8 +6,19 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope) {
+      $rootScope
+      .$on('$stateChangeStart', 
+          function(event, toState, toParams, fromState, fromParams){ 
+              $("#ui-view").html("");
+              $(".page-loading").removeClass("hidden");
+      });
 
+      $rootScope
+      .$on('$stateChangeSuccess',
+          function(event, toState, toParams, fromState, fromParams){ 
+              $(".page-loading").addClass("hidden");
+      });
     $log.debug('runBlock end');
   }
 
--- front/src/app/main/main.controller.js
+++ front/src/app/main/main.controller.js
@@ -6,9 +6,9 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($timeout, $scope, webDevTec, toastr) {
     var vm = this;
-
+    
     vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1442678867860;
@@ -16,18 +16,9 @@
     activate();
 
     function activate() {
-      getWebDevTec();
       $timeout(function() {
         vm.classAnimation = 'rubberBand';
       }, 4000);
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
     }
   }
 })();
