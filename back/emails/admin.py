5508e810dc4ab47468b3ff7195b628705be4f39d
avara1986 <a.vara.1986@gmail.com>
2015-10-08 15:25:02 +0200

    Added admin to model

--- back/emails/admin.py
+++ back/emails/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from emails.models import Contact, Email
 
 
+class ContactAdmin(admin.ModelAdmin):
+    model = Contact
+    list_display = ('email',)
+
+
+class EmailAdmin(admin.ModelAdmin):
+    model = Email
+    list_display = ('created', 'user', 'subject',)
+    list_filter = ('user', 'to',)
+
 # Register your models here.
-admin.site.register(Contact)
-admin.site.register(Email)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Email, EmailAdmin)
--- back/emails/models.py
+++ back/emails/models.py
@@ -4,16 +4,26 @@ from users.models import User
 
 
 class Contact(models.Model):
-    created = models.DateTimeField(verbose_name=_("Created date"), auto_now_add=True)
+    created = models.DateTimeField(
+        verbose_name=_("Created date"), auto_now_add=True)
     email = models.EmailField(
         verbose_name="Email", max_length=100)
 
+    def __str__(self):
+        return "%s" % self.email
+
 
 class Email(models.Model):
-    created = models.DateTimeField(verbose_name=_("Created date"), auto_now_add=True)
-    user = models.ForeignKey(User, verbose_name=_('User'), related_name='emailssends')
-    to = models.ManyToManyField(Contact, verbose_name=_('Contacts'), related_name='contacts')
+    created = models.DateTimeField(
+        verbose_name=_("Created date"), auto_now_add=True)
+    user = models.ForeignKey(
+        User, verbose_name=_('User'), related_name='emailssends')
+    to = models.ManyToManyField(
+        Contact, verbose_name=_('Contacts'), related_name='contacts')
     url = models.URLField(verbose_name=_('Url'), null=True, blank=True)
     subject = models.CharField(
         verbose_name=_("Subject"), max_length=250)
     content = models.TextField(_('Content'), null=True, blank=True)
+
+    def __str__(self):
+        return "%s" % self.subject
